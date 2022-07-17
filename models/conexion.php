d77655959ca9e7560f2dc7a7c4a00861dfc5db0c
jhonson-lc <jhoncisneros11@gmail.com>
2022-07-17 17:48:32 -0500

    fix: remote database

--- models/conexion.php
+++ models/conexion.php
@@ -1,8 +1,8 @@
 <?php
-    $servername="localhost";
-    $username="root";
-    $password="";
-    $dbname="cuartosoftware";
+    $servername="br8ci1y2h5jbgtbdlkeh-mysql.services.clever-cloud.com";
+    $username="uafziyg7bdppjgp7";
+    $password="wmg6pwxvGw8t9X1V6Nf6";
+    $dbname="br8ci1y2h5jbgtbdlkeh";
     $conn= mysqli_connect($servername,$username,$password,$dbname);
     $mysqli = new mysqli($servername,$username,$password,$dbname);
     if(!$mysqli)
--- php/conexion.php
+++ php/conexion.php
@@ -3,10 +3,10 @@
 <?php 
 	function conexion()
 	{
-	$servername="localhost";
-    $username="root";
-    $password="";
-    $dbname="cuartosoftware";
+	$servername="br8ci1y2h5jbgtbdlkeh-mysql.services.clever-cloud.com";
+    $username="uafziyg7bdppjgp7";
+    $password="wmg6pwxvGw8t9X1V6Nf6";
+    $dbname="br8ci1y2h5jbgtbdlkeh";
     return $conn= mysqli_connect($servername,$username,$password,$dbname);
 	}
 
